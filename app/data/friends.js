051670b75bb670be2d3c3d79fb62fb0d0a49ab66
JosephRivera10 <joseph.rivera.09@gmail.com>
2018-02-27 22:44:42 -0600

    friends

---
+++ app/data/friends.js
@@ -0,0 +1,39 @@
+
+var friends = [
+{
+	"name": "test",
+	"photo": "https://storage.googleapis.com/zopnow-static/images/products/320/fresh-apple-red-delicious-v-500-g.png",
+	"scores": [
+	"4",
+	"3",
+	"4",
+	"5",
+	"2",
+	"3",
+	"5",
+	"2",
+	"2",
+	"5"
+	]
+	},
+	{
+	"name": "Joe",
+	"photo": "https://www.google.com/imgres?imgurl=https://pbs.twimg.com/profile_images/552307347851210752/vrXDcTFC.jpeg&imgrefurl=https://twitter.com/joerogan&h=400&w=400&tbnid=z1p7wb2aU41bjM:&tbnh=186&tbnw=186&usg=__d-nO2W0JPy-YYplLOJy9Wku1pZU%3D&vet=1&docid=yTmRkBXi2GmUIM&itg=1&sa=X&ved=0ahUKEwjs7YHK0MfZAhUEQq0KHS2yDW8Q_B0ImQIwGw",
+	"scores": [
+	"5",
+	"2",
+	"4",
+	"1",
+	"1",
+	"5",
+	"1",
+	"1",
+	"1",
+	"5"
+	]
+	}
+
+];
+
+
+module.exports = friends;
